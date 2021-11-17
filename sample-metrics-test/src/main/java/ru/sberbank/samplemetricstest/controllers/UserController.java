e139aeceb31ae34495fd14dd1ff301aeaa539a26
Хангай Тархаев <kstarkhaev@sberbank.ru>
2021-11-17 13:55:21 +0700

    Добавлен docker-compose

--- sample-metrics-test/src/main/java/ru/sberbank/samplemetricstest/controllers/UserController.java
+++ sample-metrics-test/src/main/java/ru/sberbank/samplemetricstest/controllers/UserController.java
@@ -2,11 +2,14 @@ package ru.sberbank.samplemetricstest.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.sberbank.samplemetricstest.entities.User;
 import ru.sberbank.samplemetricstest.repository.UserRepo;
 
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepo userRepo;
@@ -16,7 +19,7 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<User> addUser(User user) {
+    public ResponseEntity<User> addUser(@RequestBody User user) {
         User save = userRepo.save(user);
         return ResponseEntity.status(201).body(save);
     }
--- sample-metrics-test/src/main/java/ru/sberbank/samplemetricstest/entities/User.java
+++ sample-metrics-test/src/main/java/ru/sberbank/samplemetricstest/entities/User.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -15,6 +17,7 @@ import javax.persistence.Id;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
