5f9d7c403c2e34a69888ad104e4d4149e732c131
Хангай Тархаев <kstarkhaev@sberbank.ru>
2021-11-17 11:18:57 +0700

    Init spring boot project

---
+++ sample-metrics-test/src/main/java/ru/sberbank/samplemetricstest/SampleMetricsTestApplication.java
@@ -0,0 +1,13 @@
+package ru.sberbank.samplemetricstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMetricsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMetricsTestApplication.class, args);
+	}
+
+}
---
+++ sample-metrics-test/src/test/java/ru/sberbank/samplemetricstest/SampleMetricsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sberbank.samplemetricstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMetricsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
